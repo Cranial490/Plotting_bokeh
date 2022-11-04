d9aa364a18d68072330630ebe351d5f7ce3584e2
Cranial490 <pranjal.paliwal490@gmail.com>
2022-11-04 10:36:05 -0400

    code formatting

--- Dimensional-Insight/scatterPlot.py
+++ Dimensional-Insight/scatterPlot.py
@@ -1,60 +1,61 @@
+import itertools
 from turtle import color
 from bokeh.plotting import figure, show, save, output_file
 from bokeh.io import export_svg, export_png
 import numpy as np
 from datamanager import DataHandler
 from bokeh.transform import factor_cmap
-from bokeh.palettes import Spectral6
-
-# N = 4000
-# data  = [[0,1,3], [1,3,52], [0,4,76], [1,43,30]]
-# # create a new plot with a title and axis labels
-# p = figure(title="Multiple glyphs example", x_axis_label="x", y_axis_label="y")
-# output_file(filename="custom_filename.html", title="Static HTML file")
-# # add multiple renderers
-# colors = ["red","yellow"]
-# for d in data:
-#     p.circle(d[1], d[0], radius=1, legend_label="Objects", line_color=colors[d[0]], size=12)
-# # p.circle(x, y1, legend_label="Objects", color="black", size=12)
-
-# # show the results
-# # export_svg(p, filename="plot.svg")
-# # export_png(p, filename="plot.png")
-# show(p)
-# # save(p)
-
+from bokeh.models import ColumnDataSource
+from bokeh.palettes import Spectral6, Spectral10
 from selenium.webdriver import Chrome, ChromeOptions
 
-options = ChromeOptions()
+class Plot:
+    def __init__(self) -> None:
+        self.colorpalette = Spectral10
+        options = ChromeOptions()
+        web_driver = Chrome(executable_path='/home/cranial/chromedriver')
+        # options.add_argument('--headless')
+        # options.add_argument("--window-size=2000x2000")
+        # metrics = { "deviceMetrics": { "pixelRatio": 1.0 } }
+        # options.add_experimental_option("mobileEmulation", metrics)
 
-options.add_argument('--headless')
-options.add_argument("--window-size=2000x2000")
-metrics = { "deviceMetrics": { "pixelRatio": 1.0 } }
-options.add_experimental_option("mobileEmulation", metrics)
-web_driver = Chrome(executable_path='/home/cranial/chromedriver')
+    def scatter(self, x , y, categories = None, palette=None):
+        p = figure(plot_width=1000, plot_height=800, title="Dummy Data")
+        if categories:
+            categorycolors = self.create_color_map(categories)
+            source = ColumnDataSource(dict(x=x, y= y, color=categorycolors, label=categories))
+            p.circle(x = 'x', y = 'y', color='color', fill_alpha=0.4, size=8, legend_group='label', source=source)
+        else:
+            p.circle(x , y , fill_alpha=0.4, size=8)
+        show(p)
+        return p
 
-import numpy as np
+    def create_color_map(self, categories, palette=None):
+        if palette:
+            self.colorpalette = palette
+        categorySet = list(set(categories))
+        colorPallete = itertools.cycle(self.colorpalette)
+        colorMap = {}
+        idx = 0
+        for shade in colorPallete:
+            if idx >= len(categorySet):
+                break
+            colorMap[categorySet[idx]] = shade
+            idx += 1
+        return [colorMap[x] for x in categories]
 
-from bokeh.plotting import figure, show, output_file
 
 dh = DataHandler()
-data = dh.read_csv("dummyData.csv")
-
-#removing header
+data = dh.read_csv("dummyData1.csv")
 data = data[1:]
-categories = [row[0] for row in data]
+categories = [row[0] for row in data]   
 id = [row[1] for row in data]
-x = [row[2] for row in data][:100]
-y = [row[3] for row in data][:100]
-print(x)
-categorySet = set(categories)
-cmap = factor_cmap('Categories', palette=Spectral6, factors=list(categorySet))
+x=[row[2] for row in data]
+y=[row[3] for row in data]
 
-p = figure(plot_width=600, plot_height=450, title="Dummy Data")
+plt = Plot()
+plt.scatter(x,y, categories)
+    
+    # from bokeh.io import export_png
 
-p.circle(x, y, color="black", fill_alpha=0.2)
-
-show(p)
-# from bokeh.io import export_png
-
-# export_png(p, filename="out.png", webdriver=web_driver)
+    # export_png(p, filename="out.png", webdriver=web_driver)
